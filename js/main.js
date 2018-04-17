ffdbcf8b1edb4583f46cdd2d8a158b56f8ca156d
Tree Casiano <tree.pdx@gmail.com>
2018-04-17 08:54:48 -0700

    Add main header menu and associated styles and event listener

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 function initialize(){
     var $infoPanel = $('.infoPanel');
     var $infoPanelToggle = $('.infoPanel__bar');
+    var $infoButton = $('#main-header-info-button');
+    var $mainHeaderMenu = $('#main-header-menu');
 
     $infoPanelToggle.click(function () {
         $infoPanel.toggle();
+    });
+
+    $infoButton.click(function () {
+        $mainHeaderMenu.slideToggle();
     });
 }
 
